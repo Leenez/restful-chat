8231a5b7b538ff4bca753539226b2bb6809c69c0
unknown <jani_leinonen@hotmail.com>
2023-07-04 04:47:52 +0300

    not tested

--- frontend/src/components/Banner.js
+++ frontend/src/components/Banner.js
@@ -1,17 +1,32 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { logout } from '../redux/chatSlice'
 import config from '../../../config'
 
 const Banner = (props) => {
 
     const info = useSelector(state => state.info)
+    const user = useSelector(state => state.user)
+    const token = useSelector(state => state.token)
 
-    return (
-        <div>
-            <div><p>{config.heading}</p></div>
-            <div><p>{info}</p></div>
-        </div>
+    const dispatch = useDispatch()
+
+    if (!user) {
+        return (
+            <div>
+                <div><p>{config.heading}</p></div>
+                <div><p>{info}</p></div>
+            </div>
     )
+    } else {
+        return (
+            <div>
+                <div><p>{config.heading}</p></div>
+                <div><p>{info}</p></div>
+                <div><button onClick={dispatch(logout(token))}>Logout</button></div>
+            </div>
+        )
+    }
 }
 
 export default Banner
